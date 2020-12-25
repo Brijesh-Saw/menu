445933a047d81f7597ab10b1423b8417ba2202b8
Brijesh-Saw <sawbrijesh7@gmail.com>
2020-12-25 23:33:06 +0530

    primary

---
+++ lib/categories_Screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:menu/category_item.dart';
+import 'package:menu/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Menu"),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(10),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(
+                  catData.id,
+                  catData.title,
+                  catData.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/dummy_data.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:menu/models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: "c1",
+    title: "Indian",
+    color: Colors.purple,
+  ),
+  Category(
+    id: "c2",
+    title: "Starter",
+    color: Colors.red,
+  ),
+  Category(
+    id: "c3",
+    title: "Chinies",
+    color: Colors.blue,
+  ),
+  Category(
+    id: "c4",
+    title: "Italian",
+    color: Colors.green,
+  ),
+  Category(
+    id: "c5",
+    title: "Fast Food",
+    color: Colors.yellow,
+  ),
+];

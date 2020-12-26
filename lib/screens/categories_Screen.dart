cd96b02d35f85d6a48f4ee54e3ab9ba1c321b7d2
Brijesh-Saw <sawbrijesh7@gmail.com>
2020-12-27 03:55:22 +0530

    initial commit

--- lib/screens/categories_Screen.dart
+++ lib/screens/categories_Screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:menu/widgets/category_item.dart';
-import 'package:menu/dummy_data.dart';
+import '../widgets/category_item.dart';
+import '../dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override

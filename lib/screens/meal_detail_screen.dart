80fb86a29915d4009e487ef09b79a7d7210220e7
Brijesh-Saw <sawbrijesh7@gmail.com>
2020-12-28 19:00:34 +0530

    initial commit

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -4,6 +4,16 @@ import '../dummy_data.dart';
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
 
+  Widget buildSelectionTitle(BuildContext context, String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.headline1,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final mealid = ModalRoute.of(context).settings.arguments as String;
@@ -21,25 +31,28 @@ class MealDetailScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
+        buildSelectionTitle(context, "INGREDIENTS"),
         Container(
-          margin: EdgeInsets.symmetric(vertical: 10),
-          child: Text(
-            'Ingredients',
-            style: Theme.of(context).textTheme.headline1,
-          ),
-        ),
-        Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10)),
+          margin: EdgeInsets.all(3),
+          padding: EdgeInsets.all(3),
           height: 200,
           width: 300,
           child: ListView.builder(
             itemBuilder: (ctx, index) => Card(
-              color: Theme.of(context).accentColor,
-              child: Text(selectedMeal.ingredients[index]),
+              color: Theme.of(context).primaryColor,
+              child: Container(
+                padding: EdgeInsets.all(4),
+                child: Text("  ${selectedMeal.ingredients[index]}"),
+              ),
             ),
             itemCount: selectedMeal.ingredients.length,
           ),
         ),
-        Text("The Meals! - $mealid!"),
+        buildSelectionTitle(context, "Steps"),
       ]),
     );
   }
